2c88f0528b9ebc6038d7e36a57e7e54b709c776a
NicoSalva <98570371+NMS47@users.noreply.github.com>
2022-04-01 23:34:18 +0200

    Update Scoreboard.py

--- Scoreboard.py
+++ Scoreboard.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 
-LIMIT = 370
+with open('high_score.txt') as t:
+    HIGH_SCORE = int(t.read())
 
 
 class ScoreBoard(Turtle):
@@ -8,14 +9,26 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = HIGH_SCORE
         self.color("white")
         self.penup()
         self.hideturtle()
-        self.sety(320)
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 20, "normal"))
+        self.sety(240)
+        self.new_score()
 
     def new_score(self):
         self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", align="center", font=("Arial", 20, "normal"))
         self.score += 1
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 20, "normal"))
+
+    def new_high_score(self):
+        print(self.score)
+        print(self.high_score)
+        if self.high_score < self.score:
+            self.high_score = self.score
+            with open('high_score.txt', mode='w') as text:
+                text.write(str(self.score))
+                print("escribido")
+
+
 
